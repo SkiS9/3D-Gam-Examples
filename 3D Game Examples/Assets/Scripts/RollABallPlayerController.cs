354095620dbbd1bbef81ee3f58c8c3097fb16bed
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2024-01-30 14:24:42 -0800

    Finished on Basic Movement Starting Jumping

---
+++ 3D Game Examples/Assets/Scripts/RollABallPlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollABallPlayerController : MonoBehaviour
+{
+    
+    public GameObject WinText;
+
+    public float Speed = 100f;
+
+    private float _horizontalinput;
+    private float _forwardinput;
+
+    private int _count;
+
+    private Rigidbody _playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _count = 0;
+        
+        WinText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalinput = Input.GetAxis("Horizontal");
+        _forwardinput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_horizontalinput, 0.0f, _forwardinput);
+
+        _playerRigidbody.AddForce(movement * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            _count = _count + 1;
+            
+
+            if (_count >= 8)
+            {
+                WinText.gameObject.SetActive(true);
+            }
+        }
+
+    }
+}
